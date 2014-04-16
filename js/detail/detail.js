d6485a40ff6cf6f0d9740de4cd02100673c8af5f
Circle <a15795335@163.com>
2014-04-17 00:40:53 +0800

    详情页搞定，坐等联调 （ps： 没有加多余的内容）

--- js/detail/detail.js
+++ js/detail/detail.js
@@ -9,18 +9,46 @@ define('detail/detail', ['common/common', 'server/server'], function($, server) 
 		price: "?",
 		address: "?",
 		square: "?",
-		brief: "?",
 		wc_condition_id: "?",
 		network_condition_id: "?",
-		ac_id: "?"
+		ac_id: "?",
+		img_path:[]
 	};
 
-	function getDetailData(str,callback) {
+	function getDetailData(str, callback) {
 		bizObj.room_id = str;
 		$.J_xhr(server.detail, bizObj, function(data) {
 			//回调函数
-			callback && callback($.J_json.parse(data));
+			callback && callback(mapData($.J_json.parse(data)));
 		})
+	}
+
+	function mapData(data) {
+		var result = {};
+		for (var i in data) {
+			if (i == "wc_condition_id" || i == "ac_id" || i == "hot_water_id") {
+				result[i] = data[i] == 1 ? "有" : "无";
+			} else if (i == "network_condition_id") {
+				if (data[i] == 1) {
+					result[i] = "有线网络";
+				} else if (data[i] == 2) {
+					result[i] = "无线网络";
+				} else if (data[i] == 3) {
+					result[i] = "有线及无线网络"
+				}
+			} else if (i == "img_path") {
+				result["img_path"] = [];
+				for (var j in data[i]) {
+					result["img_path"].push({
+						url: data[i][j]
+					});
+				}
+				bizObj.img_path = result["img_path"];
+			} else {
+				result[i] = data[i];
+			}
+		}
+		return result;
 	}
 
 	return {
--- js/detail/main.js
+++ js/detail/main.js
@@ -5,15 +5,56 @@ require(['common/common', 'detail/detail'], function($, detail) {
 
 	//拿到初始化数据并渲染
 	detail.getDetailData(bizObj.room_id, function(data) {
+		var imgLength = data.img_path.length;
 		$.J_apply($('.J_template_detail'), data, function() {
-			//动画效果
-			var imgShow = $('.imgShow');
 
+
+
+			var imgShow = $('.imgShow');
+			var divAbsolute = $('.sliderAbsolute', imgShow);
+			var imgW = 88;
+
+			// 给图片添加active
+			if (bizObj["img_path"].length <= 0) {
+				return;
+			}
+
+			$('.sliderAbsolute .item:eq(0)', imgShow).addClass('active');
+			$('.show', imgShow).attr("src", bizObj["img_path"][0].url);
+
+			//动画效果
 			$('.sliderAbsolute .item', imgShow).click(function() {
 				$(this).parent().children('.item').removeClass('active');
 				$(this).addClass('active');
 				$('.show', imgShow).attr('src', $(this).children('img').attr("src"));
 			});
+
+			$('.slider .direction', imgShow).click(function() {
+				var translateX;
+				if ($(this).hasClass("left")) {
+					translateX = translate(parseInt(divAbsolute.css('left')), +1);
+				} else if ($(this).hasClass("right")) {
+					translateX = translate(parseInt(divAbsolute.css('left')), -1);
+				}
+
+				if (translateX === false) {
+					return;
+				}
+				divAbsolute.animate({
+					left: translateX.toString() + "px"
+				});
+			});
+
+			function translate(val, num) {
+				val = val + num * imgW;
+				var l = imgLength * imgW - 4 * imgW;
+				if (val < -1 * l) {
+					return false;
+				} else if (val > 0) {
+					return false;
+				}
+				return val;
+			}
 
 		});
 
